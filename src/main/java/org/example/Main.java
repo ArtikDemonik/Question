d4e4bbdac65497a82cfea8f3f009e1211764b7b9
ArtikDemonik <artikdemonik@yandex.ru>
2023-01-28 09:19:53 +0300

    add all code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Type firs number: ");
-        int num = in.nextInt();
-        while (num!=1){
-            System.out.printf("%d, ", num);
-            if (num%2==0)
-                num/=2;
-            else
-                num = num*3+1;
+        System.out.print("Type number: ");
+        int number = in.nextInt();
+        if (number%2!=0){
+            return;
         }
-        System.out.println(1);
+        int first = 1;
+        int second = number-first;
+        while (!(isPrime(first)&isPrime(second))){
+            first++;
+            second = number-first;
+        }
+        System.out.printf("First: %d, Second: %d", first, second);
+    }
+    static boolean isPrime(int num){
+        for(int i = 2 ; i < num; i++){
+            if(num%i==0)
+                return false;
+        }
+        return true;
     }
 }
